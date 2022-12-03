e561dd998bd390c217d35b0eeadc8f5e411be535
Mahmoud Mohamed <mahmoudm.abdelwadood@gmail.com>
2022-12-03 19:03:08 +0200

    handle settings and terms fragment

--- app/src/main/java/com/grand/navigation/MainActivity.kt
+++ app/src/main/java/com/grand/navigation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.grand.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.onNavDestinationSelected as onNavDestinationSelected1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -21,6 +24,21 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(item.itemId == R.id.termsFragment) {
+            val action = NavGraphDirections.actionGlobalTermsFragment()
+            navController.navigate(action)
+            true
+        }else{
+            item.onNavDestinationSelected1(navController) || super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/grand/navigation/TermsFragment.kt
@@ -0,0 +1,8 @@
+package com.grand.navigation
+
+import androidx.fragment.app.Fragment
+
+
+class TermsFragment : Fragment(R.layout.fragment_terms) {
+
+}
